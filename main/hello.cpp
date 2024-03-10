15f0a8762904f91be5126adbaca6c6d5ea4ef0db
Shreeja R <92515472+Mystery510@users.noreply.github.com>
2024-03-10 21:01:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Just changes\n";
+  cout << "Just changes";
   return 0;
 }
